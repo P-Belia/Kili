53a4a7efde8ac5f7876384c3eb124619b11db5a5
P-Belia <118549811+P-Belia@users.noreply.github.com>
2023-05-20 13:22:55 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,157 @@
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
+
+// Home page imgevenement effect
+
+window.addEventListener('load', function() {
+  var image = document.getElementById('imgevenement');
+  
+  // Retirer la classe pour le zoom in après 3 secondes
+  setTimeout(function() {
+    image.classList.remove('zoom-in');
+    image.classList.add('zoom-out');
+  }, 1000);
+});
+
+// Animation image
+
+const scrollElements2 = document.querySelectorAll(".zoom-in2");
+
+const elementInView2 = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView2 = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement2 = (element) => {
+  element.classList.add("zoom-in2");
+};
+
+const hideScrollElement2 = (element) => {
+  element.classList.remove("zoom-in2");
+};
+
+const handleScrollAnimation2 = () => {
+  scrollElements2.forEach((el) => {
+    if (elementInView2(el, 1.35)) {
+      displayScrollElement2(el);
+    } else if (elementOutofView2(el)) {
+      hideScrollElement2(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation2();
+});
+
+
+
+
+/* const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+  const windowWidth = window.innerWidth || document.documentElement.clientWidth;
+
+  if (windowWidth < 412 && windowHeight < 883) {
+    dividend = 1.1;
+  }
+
+  return elementTop <= windowHeight / dividend;
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+  return elementTop > windowHeight;
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el);
+    }
+  });
+};
+
+window.addEventListener("scroll", handleScrollAnimation);
+
+/*Home page imgevenement effect
+
+window.addEventListener("load", () => {
+  const imgevenement = document.createElement("img");
+  imgevenement.src = "./evenement/journeefemmes-180323.jpeg"
+
+  imgevenement.style.position = "fixed";
+  imgevenement.style.top = "50%";
+  imgevenement.style.left = "50%";
+  imgevenement.style.transform = "translate(-50%, -50%)";
+  imgevenement.style.animation = "zoom 3s infinite alternate";
+
+  document.body.appendChild(imgevenement);
+
+  setTimeout(() => {
+    imgevenement.style.animation = "none";
+  }, 3000);
+});*/
